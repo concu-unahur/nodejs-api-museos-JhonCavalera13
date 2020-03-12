c01045a2b2aace3eb2fc41fafa7d642814938663
Jonathan.ybarra <jonathan.ybarra@gmail.com>
2020-03-12 20:24:48 -0300

    f

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
 const superagent = require('superagent');
+const fs = require('fs');
 
-function imprimirMuseos(error, respuesta) {
+//MapQuest api
+const end = (err,data) => {
+  if (err) console.log('Error');
+  console.log(data.toString());
+  console.log('ejecución finalizada');
+}
+
+const leer = () => {
+  fs.readFile('organismosMuseos.txt',end)
+}
+
+const imprimirMuseos = (error, respuesta) => {
   if (error) {
     throw new Error('algo se rompió', error);
   }
-
-  const cantidad = respuesta.body.count;
   const museos = respuesta.body.results;
-
-  console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
+  const mapMuseos = museos.map(m => (
+    console.log('escribiendo museo en archivo'),
+    `Museo: ${m.nombre}, ${m.direccion}. Por cualquier consulta comunicarse al ${m.telefono}\n`
+  ));
+  fs.appendFile('organismosMuseos.txt', mapMuseos, requestOrganismos);
 }
 
-console.log('Antes de llamar a superagent')
+const imprimirOrganismos = (error, respuesta) => {
+  if (error) {
+    throw new Error('algo se rompió', error);
+  }
+  const organismos = respuesta.body.results;
+  const mapOrganismos = organismos.map(o => (
+    console.log('escribiendo organismo en archivo'),
+    `Organismo: ${o.direccion}. Por cualquier consulta comunicarse al ${o.telefono}\n`
+  ));
+  fs.appendFile('organismosMuseos.txt', mapOrganismos, leer);
+}
 
-superagent
-  .get('https://www.cultura.gob.ar/api/v2.0/museos')
-  .query({ format: 'json' })
-  .end(imprimirMuseos)
+const requestOrganismos = () => {
+  superagent
+    .get('https://www.cultura.gob.ar/api/v2.0/organismos')
+    .query({ format: 'json' })
+    .end(imprimirOrganismos)
+  console.log('Descargando Organismos');
+}
 
-console.log('Después de llamar a superagent')
+const requestMuseos = () => {
+  superagent
+    .get('https://www.cultura.gob.ar/api/v2.0/museos')
+    .query({ format: 'json' })
+    .end(imprimirMuseos)
+  console.log('Descargando Museos');
+}
+
+requestMuseos()
